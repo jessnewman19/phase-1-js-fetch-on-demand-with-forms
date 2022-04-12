e77d315655e4539075989f043326fe3b061d4e19
Jessica <jess.newman19@gmail.com>
2022-04-12 13:03:07 -0400

    commit tree c3e1b978a07583ff903afc12891032a7c93920c4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', (e) => { 
+    e.preventDefault() //Prevent page from automatically refreshing
+
+    //1. find the target
+    console.log(e) //Console's the event. Target = "searchByID"
+
+    //2. create variable with the input value
+    const input = e.target.children[1].value
+    console.log(input) //returns "hello"
+
+    fetch(`http://localhost:3000/movies/${input}`)
+    .then(response => response.json())
+    .then(data => { 
+      const title = document.querySelector('#movieDetails h4')
+      const summary = document.querySelector('#movieDetails p')
+
+      title.innerText = data.title
+      summary.innerText = data.summary
+    })
+
+    inputForm.reset()
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
